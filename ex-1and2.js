4d6f6ae792cdf1e2de9a56dc34b9b4b6cc151686
chaijiang <chaijiangchaisan@gmail.com>
2025-04-10 18:18:25 +0700

    edit:ex1,2,3,4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,32 @@
-//Exercise #1: Variable Creation
-//Start coding here
+//Exercise #1: Variable 
+
+//1.1
+let firstName = Karter ; 
+//1.2
+let lastName = Anderson; 
+//1.3
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"; 
+//1.4
+const restaurantName = "Chef's Table by Chef James";
+//1.5
+const foodPrice = 15000; 
+//1.6
+const numberOfOpenRestaurants = 1500; 
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+//2.1
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641" ;
+//2.2
+foodPrice = 5000;
+//2.3
+numberOfOpenRestaurants = 2500;
+
+//test
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+console.log(restaurantName);
+console.log(foodPrice);
+console.log(numberOfOpenRestaurants);
